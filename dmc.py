df0a8be71b17475b6c5318319b2cc0711f3d75f2
Logan Ritter <32370525+loganritter@users.noreply.github.com>
2024-08-30 23:42:50 -0400

    Update dmc.py
    - implemented N > 1 particles
    - improved progress bar
    - vectorized averagePotentialEnergy and walk functions
    - optimized branch function to only replicate and remove points as needed
    - removed multiple calculation of m

--- dmc.py
+++ dmc.py
@@ -1,238 +1,225 @@
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 
 class DiffusionMonteCarlo:
-  def __init__(self, d, n, steps=1000, N0=500, Nmax=2000, num_buckets=200, x_min=-20.0, x_max=20.0, dt=0.1, alpha=10.0):
-    """
-    __init__ initializes base class
+    def __init__(self, d, n, steps=1000, N0=500, Nmax=2000, num_buckets=200, x_min=-20.0, x_max=20.0, dt=0.1, alpha=10.0):
+        """
+        __init__ initializes base class
 
-    d: Spatial dimension
-    n: Number of particles
-    steps: Number of time steps to run
-    N0: Initial number of replicas
-    Nmax: Maximum number of replicas
-    num_buckets: Number of bins
-    x_min: Lower bound of grid
-    x_max: Upper bound of grid
-    dt: Time step
-    alpha: Emperically chosen positive parameter to update energy, based on 1/dt
-    """
-    self.d = d
-    self.n = n
-    self.steps = steps
-    self.dn = self.d * self.n # Effective dimension
-    self.N0 = N0
-    self.N = N0 # Variable to allow number of replicas to change
-    self.Nmax = Nmax
-    self.num_buckets = num_buckets
-    self.x_min = x_min
-    self.x_max = x_max
-    self.dt = dt
-    self.alpha = alpha
+        d: Spatial dimension
+        n: Number of particles
+        steps: Number of time steps to run
+        N0: Initial number of replicas
+        Nmax: Maximum number of replicas
+        num_buckets: Number of bins
+        x_min: Lower bound of grid
+        x_max: Upper bound of grid
+        dt: Time step
+        alpha: Emperically chosen positive parameter to update energy, based on 1/dt
+        """
+        self.d = d
+        self.n = n
+        self.steps = steps
+        self.dn = self.d * self.n # Effective dimension
+        self.N0 = N0
+        self.N = N0 # Variable to allow number of replicas to change
+        self.Nmax = Nmax
+        self.num_buckets = num_buckets
+        self.x_min = x_min
+        self.x_max = x_max
+        self.dt = dt
+        self.alpha = alpha
 
-    self.flags = np.zeros(self.Nmax, dtype=int) # Three integer types: 0 = dead, 1 = alive, 2 = newly made replica
-    for i in range(self.N):
-      self.flags[i] = 1 # Set the flags of initial replicas to "alive"
-    self.points = np.zeros((self.Nmax, self.dn)) # (Initial) positions of replicas, shape: (Nmax, dn)
-    self.E1 = self.averagePotentialEnergy() # Reference energy (1)
-    self.E2 = self.E1 # Reference energy (2)
-    self.energy_storage = [] # Storage list to calculate average reference energies at the end
-    self.hist_storage = np.zeros(self.num_buckets, dtype=int) # Histogram storage array
+        self.flags = np.zeros(self.Nmax, dtype=int) # Three integer types: 0 = dead, 1 = alive, 2 = newly made replica
+        for i in range(self.N):
+          self.flags[i] = 1 # Set the flags of initial replicas to "alive"
+        self.points = np.zeros((self.Nmax, self.dn)) # (Initial) positions of replicas, shape: (Nmax, dn)
+        self.E1 = self.averagePotentialEnergy() # Reference energy (1)
+        self.E2 = self.E1 # Reference energy (2)
+        self.energy_storage = [] # Storage list to calculate average reference energies at the end
+        self.hist_storage = np.zeros(self.num_buckets, dtype=int) # Histogram storage array
 
-  def U(self, x):
-    """
-    U: potential energy (for a single replica)
+    def U(self, x):
+        """
+        U: potential energy (for a single replica)
 
-    x: array shape (d), position of replica
+        x: array shape (d), position of replica
 
-    returns: float, energy of the replica
-    """
-    pot = 0.5 * np.linalg.norm(x)**2
-    return pot
+        returns: float, energy of the replica
+        """
+        pot = 0.5 * np.linalg.norm(x)**2
+        return pot
 
-  def averagePotentialEnergy(self):
-    """
-    potential energy (for the entire system/all the replicas)
+    def averagePotentialEnergy(self):
+        """
+        Calculate the potential energy (for the entire system/all the replicas)
+        using vectorized NumPy operations.
 
-    returns: float, energy of all replicas
-    """
-    pot = 0.0
-    for i in range(self.Nmax):
-      if self.flags[i]:
-        pot += self.U(self.points[i])
-    avg_pot = pot / self.N
+        Returns:
+            float: Average energy of all replicas
+        """
+        alive_indices = self.flags == 1
+        pot = 0.5 * np.sum(np.linalg.norm(self.points[alive_indices], axis=1) ** 2)
+        avg_pot = pot / self.N
+        return avg_pot
 
-    return avg_pot
 
-  def walk(self):
-    """
-    walk: moves points around steps once
+    def walk(self):
+        """
+        Vectorized walk: moves points around once.
+        """
+        self.points += np.sqrt(self.dt) * rng.normal(0, 1, size=(self.Nmax, self.dn))
 
-    returns nothing
-    """
-    self.points += np.sqrt(self.dt) * np.random.normal(0, 1, size=(self.Nmax, self.dn))
+    def calculateM(self, E_ref, index):
+        """
+        calculateM: calculates m :)
 
-  def calculateM(self, E_ref, index):
-    """
-    calculateM: calculates m :)
+        E_ref: float, reference energy
+        index: (nd), position of replica
 
-    E_ref: float, reference energy
-    index: (nd), position of replica
+        returns: int, what to do with the replica in branch()
+        """
+        energy = self.U(self.points[index])
+        m = int(1 - (energy - E_ref) * self.dt + np.random.uniform(0, 1))
+        return m
+        
+    def replicateSingleReplica(self, i):
+        """
+        replicate point i into next available point, update flags by 2 to not loop over new replica
+        i: index of points[i]
 
-    returns: int, what to do with the replica in branch()
-    """
-    return int(1 - (self.U(self.points[index]) - E_ref)*self.dt + np.random.uniform(0, 1) )
+        returns nothing
+        """
+        for j in range(self.Nmax):
+            if self.flags[j] == 0:
+                self.flags[j] = 2
+                self.points[j] = self.points[i]
+                self.N += 1
+                break
 
-  def replicateSingleReplica(self, i):
-    """
-    replicate point i into next available point, update flags by 2 to not loop over new replica
-    i: index of points[i]
+    def branch(self):
+        """
+        branch: replicates and removes points from the distribution as needed.
+        """
+        alive_indices = np.where(self.flags == 1)[0]  # Get indices of alive replicas
 
-    returns nothing
-    """
-    for j in range(self.Nmax):
-      if self.flags[j] == 0:
-        self.flags[j] = 2
-        self.points[j] = self.points[i]
-        self.N += 1
-        break
+        # Calculate m for all alive replicas at once
+        energies = np.array([self.U(self.points[i]) for i in alive_indices])
+        m_values = np.floor(1 - (energies - self.E2) * self.dt + np.random.uniform(0, 1, len(alive_indices)))
 
-  def branch(self):
-    """
-    branch: replicates and removes points from the distribution as needed
+        # Process each replica based on its m value
+        for i, m in zip(alive_indices, m_values):
+            if m <= 0:  # KILL
+                self.flags[i] = 0
+                self.N -= 1
+            elif m == 2:  # Replicate once
+                self.replicateSingleReplica(i)
+            elif m >= 3:  # Replicate twice if m >= 3
+                self.replicateSingleReplica(i)
+                self.replicateSingleReplica(i)
 
-    returns nothing
-    """
-    for i in range(self.Nmax): # Loop through all replicas
-      if self.flags[i] == 1: # If replica is alive, calculate m
-        m = self.calculateM(self.E2, i)
+        # Update energy and set new replicas to alive
+        self.flags[self.flags == 2] = 1
+        self.E1 = self.E2
+        self.E2 = self.E1 + self.alpha * (1.0 - self.N / self.N0)
 
-        if m == 0: # KILL
-          self.flags[i] = 0
-          self.N -= 1
-        elif m == 1: # Do nothing
-          pass
-        elif m == 2: # Replicate once
-          self.replicateSingleReplica(i)
-        else: # Replicate twice if m >= 3
-          self.replicateSingleReplica(i)
-          self.replicateSingleReplica(i)
-    
-    for i in range(self.Nmax): # Set previously created replica to "alive"
-      if self.flags[i] == 2:
-        self.flags[i] = 1
+    def bucketNumber(self, x):
+        """
+        bucketNumber: returns the index of the bucket that x falls in the interval [x_min, x_max] for num_buckets buckets
 
-    self.E1 = self.E2
-    self.E2 = self.E1 + self.alpha*(1.0 - self.N / self.N0)
+        x: float or array-like, position of replica
 
-  def bucketNumber(self, x):
-    """
-    bucketNumber: returns the index of the bucket that x falls in the interval [x_min, x_max] for num_buckets buckets
+        return: int, bucket number
+        """
+        # Convert to the scalar range if x is an array
+        x_val = np.linalg.norm(x) if isinstance(x, np.ndarray) else x
+        
+        if x_val < self.x_min:
+            return 0
+        elif x_val > self.x_max:
+            return int(self.num_buckets - 1)
+        else:
+            return int((x_val - self.x_min) * self.num_buckets / (self.x_max - self.x_min))
 
-    x: float, position of replica
+    def count(self):
+        """
+        count: counts the number of replicas in each bucket
 
-    return: int, bucket number
-    """
-    if x < self.x_min:
-      return 0
-    elif x > self.x_max:
-      return int(self.num_buckets - 1)
-    else:
-      return int((x - self.x_min)*self.num_buckets / (self.x_max - self.x_min))
+        returns nothing
+        """
+        self.energy_storage.append(self.averagePotentialEnergy())
+        
+        for i in range(self.Nmax):
+            if self.flags[i]:
+                for j in range(self.dn):
+                    # Use self.points[i, j] for the j-th dimension
+                    self.hist_storage[self.bucketNumber(self.points[i, j])] += 1
 
-  def count(self):
-    """
-    count: counts the number of replicas in each bucket
+    def output(self):
+        """
+        output: outputs stuff to the screen
 
-    returns nothing
-    """
-    self.energy_storage.append(self.averagePotentialEnergy())
-    
-    for i in range(self.Nmax):
-      if self.flags[i]:
-        for j in range(self.dn):
-          self.hist_storage[self.bucketNumber(self.points[i])] += 1
+        returns nothing
+        """
+        avg_energy = np.average(np.array(self.energy_storage))
+        print("Average Reference Energy: {}".format(avg_energy))
+        print("Analytic Energy:          {}".format(0.5)) # Exact energy for the 1D ground state harmonic oscillator in dimensionless units
+        print("Percent Difference:       {:.2f}%".format(np.abs(0.5 - avg_energy)/0.5*100))
 
-  def output(self):
-    """
-    output: outputs stuff to the screen
+        ax = plt.gca()
 
-    returns nothing
-    """
-    avg_energy = np.average(np.array(self.energy_storage))
-    print("Average Reference Energy: {}".format(avg_energy))
-    print("Analytic Energy:          {}".format(0.5)) # Exact energy for the 1D ground state harmonic oscillator in dimensionless units
-    print("Percent Difference:       {:.2f}%".format(np.abs(0.5 - avg_energy)/0.5*100))
+        ax.xaxis.set_minor_locator(AutoMinorLocator())
+        ax.yaxis.set_minor_locator(AutoMinorLocator())
+        ax.tick_params(which='both', axis="x", direction="in")
+        ax.tick_params(which='both', axis="y", direction="in")
+        ax.tick_params(which='both', bottom=True, top=True, left=True, right=True)
 
-    ax = plt.gca()
+        plt.rcParams["figure.figsize"] = [10.00, 7.00]
+        plt.rcParams.update({'font.size': 14})
 
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    ax.tick_params(which='both', axis="x", direction="in")
-    ax.tick_params(which='both', axis="y", direction="in")
-    ax.tick_params(which='both', bottom=True, top=True, left=True, right=True)
+        plt.title('Reference Energy Per Time Step')
+        plt.xlabel(r'$\tau$')
+        plt.ylabel('<$E_{R}$>')
+        x = [i for i in range(len(self.energy_storage))]
+        plt.plot(x, self.energy_storage)
+        plt.show()
 
-    plt.rcParams["figure.figsize"] = [10.00, 7.00]
-    plt.rcParams.update({'font.size': 14})
+        count = []
+        bins = []
+        for i, value in enumerate(self.hist_storage):
+            count.append(self.x_min + (self.x_max - self.x_min) * (i+0.5) / self.num_buckets)
+            bins.append(value / np.max(self.hist_storage))
+        plt.title('Ground State Wavefunction')
+        plt.xlabel('x')
+        plt.ylabel(r'$\Phi_{0}(x)$')
+        plt.bar(count, bins)
+        plt.show()
 
-    plt.title('Reference Energy Per Time Step')
-    plt.xlabel(r'$\tau$')
-    plt.ylabel('<$E_{R}$>')
-    x = [i for i in range(len(self.energy_storage))]
-    plt.plot(x, self.energy_storage)
-    plt.show()
+    def simulate(self):
+        """
+        Do the simulation
+        """
+        progress = tqdm(total=self.steps, desc="Simulating", unit="step")
+        
+        current_step = 0
+        while current_step < self.steps:
+            self.walk()
+            self.branch()
+            self.count()
+            current_step += 1
+            
+            progress.update(1)
 
-    count = []
-    bins = []
-    for i, value in enumerate(self.hist_storage):
-      count.append(self.x_min + (self.x_max - self.x_min) * (i+0.5) / self.num_buckets)
-      bins.append(value / np.max(self.hist_storage))
-    plt.title('Ground State Wavefunction')
-    plt.xlabel('x')
-    plt.ylabel('$\Phi_{0}$(x)')
-    plt.bar(count, bins)
-    plt.show()
-
-  def simulate(self):
-    """
-    Do the simulation
-    """
-    current_step = 0
-    progress = int(self.steps / 10)
-    while current_step < self.steps:
-
-      if(current_step == progress):
-        print("[ 10 |", end="", flush=True)
-      elif(current_step == 2*progress):
-        print(" 20 |", end="", flush=True)
-      elif(current_step == 3*progress):
-        print(" 30 |", end="", flush=True)
-      elif(current_step == 4*progress):
-        print(" 40 |", end="", flush=True)
-      elif(current_step == 5*progress):
-        print(" 50 |", end="", flush=True)
-      elif(current_step == 6*progress):
-        print(" 60 |", end="", flush=True)
-      elif(current_step == 7*progress):
-        print(" 70 |", end="", flush=True)
-      elif(current_step == 8*progress):
-        print(" 80 |", end="", flush=True)
-      elif(current_step == 9*progress):
-        print(" 90 |", end="", flush=True)
-      elif(current_step == 10*progress - 1):
-        print(" 100 ]\n", flush=True)
-
-      self.walk()
-      self.branch()
-      self.count()
-      current_step += 1
-
-    print("****************************************************")
-    self.output()
+        progress.close()
+        print("****************************************************")
+        self.output()
 
 # The inital parameters indicated in __init__ are ones the reference suggests but the 
 # below parameters produce a more "stable" energy curve. Takes ~12 min
 # Curve continues to flatten with increasing replicas (N0, Nmax)
-DMC = DiffusionMonteCarlo(1, 1, steps=2000, dt=0.01, x_min=-5.0, x_max=5.0, N0=10000, Nmax=50000)
-DMC.simulate()
+if __name__ == '__main__':
+    rng = np.random.default_rng()
+    DMC = DiffusionMonteCarlo(1, 1, steps=2000, dt=0.01, x_min=-5.0, x_max=5.0, N0=10000, Nmax=50000)
+    DMC.simulate()
